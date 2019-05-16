9db00c4ed24267a859ce0e76ad89101b26b67460
juniorstreichan <juniorstreichan@gmail.com>
2019-05-16 12:19:31 -0400

    💅 Adding Semantic UI theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 const App: React.FC = () => {
-  return <div>SHOPPING CART</div>;
+  return (
+    <div>
+      SHOPPING CART
+      <br />
+      <Button size="massive" icon="cart" />
+    </div>
+  );
 };
 
 export default App;
